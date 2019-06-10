161a5a24a6474e5d414f6566389f3f2e745cf2bf
NitzanMaarek <maarekn@post.bgu.ac.il>
2019-06-10 23:36:47 +0300

    Navigation with hide and show using angularjs works. Need to work on registration form validation. Need to check login routing json to server and authenticating while saving the login token on the client.

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 let app = angular.module('myApp', []);
-app.controller('myController', function($scope) {
-    $scope.loginDiv = true;
-    $scope.registerDiv = false;
-    $scope.restorePWDiv = false;
-    $scope.searchDiv = false;
-    $scope.showLogin = function () {
-        $scope.loginDiv = true;
-        $scope.registerDiv = false;
-        $scope.restorePWDiv = false;
-        $scope.searchDiv = false;
-    };
+app.controller('myController', ['$scope', function($scope) {
     $scope.showRegister = function () {
         $scope.loginDiv = false;
         $scope.registerDiv = true;
         $scope.restorePWDiv = false;
         $scope.searchDiv = false;
+        $window.alert("reg");
+    };
+    $scope.showLogin = function () {
+        $scope.loginDiv = true;
+        $scope.registerDiv = false;
+        $scope.restorePWDiv = false;
+        $scope.searchDiv = false;
+        $window.alert("login");
     };
     $scope.showSearch = function () {
         $scope.loginDiv = false;
         $scope.registerDiv = false;
         $scope.restorePWDiv = false;
         $scope.searchDiv = true;
+        $window.alert("search");
     };
     $scope.showRestorePW = function () {
         $scope.loginDiv = false;
         $scope.registerDiv = false;
         $scope.restorePWDiv = true;
         $scope.searchDiv = false;
+        $window.alert("restore");
     };
     $scope.loginCheck = function(){
 
     };
-});
+}]);
