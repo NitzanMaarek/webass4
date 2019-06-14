379a7c9c37cd5fddbdec4205057d8a37c02970b8
chenyanai <chenyanai@gmail.com>
2019-06-14 19:29:34 +0300

    added login

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let app = angular.module('myApp', []);
-app.controller('myController', ['$scope', function($scope) {
+let api_url = 'http://localhost:3000/';
+
+app.controller('myController', ['$scope', '$http', function($scope, $http) {
+
+    self.url = api_url;
+
     $scope.showRegister = function () {
         $scope.loginDiv = false;
         $scope.registerDiv = true;
@@ -19,7 +24,7 @@ app.controller('myController', ['$scope', function($scope) {
         $scope.registerDiv = false;
         $scope.restorePWDiv = false;
         $scope.searchDiv = true;
-        $window.alert("search");
+        alert("search");
     };
     $scope.showRestorePW = function () {
         $scope.loginDiv = false;
@@ -28,7 +33,32 @@ app.controller('myController', ['$scope', function($scope) {
         $scope.searchDiv = false;
         $window.alert("restore");
     };
-    $scope.loginCheck = function(){
 
+    $scope.loginCheck = function(){
+        let username = $scope.login_username;
+        let password = $scope.login_pw;
+
+        let data = {
+            userName: username,
+            password: password
+        };
+
+        let token = $http.post('http://localhost:3000/login', data);
+
+        // let req = {
+        //     method: 'POST',
+        //     url: 'http://localhost:3000/login',
+        //     headers: {
+        //         'content-Type': undefined
+        //     },
+        //     body: {
+        //         userName: username,
+        //         password: password
+        //     }
+        // }
+
+        // let token = $http(req);
+        return token;
     };
+
 }]);
